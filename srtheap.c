bf154645fa732df844f7252496b0f5394cbaadf1
Noah Christopher <53131326+NSChristopher@users.noreply.github.com>
2022-08-07 11:41:49 -0400

    Heap sort implementation complete
    Heap sort implemented and tested

--- srtheap.c
+++ srtheap.c
@@ -2,33 +2,37 @@
 #include <string.h>
 #include "srt.h"
 
-void heapify(char *base, size_t nelem, size_t size, size_t parent, int (*compar)(const void *, const void *));
+void heapify(void *base, size_t nelem, size_t size, size_t parent, int (*compar)(const void *, const void *));
 
 void srtheap(void *base, size_t nelem, size_t size, int (*compar)(const void *, const void *))
 {
     size_t i;
 
-    for (i = nelem / 2; i >= 1; i--)
+    for (i = (nelem / 2) - 1; i > 0; i--)
     {
-        heapify(base, nelem, size, i, compar);
+        heapify(base, nelem - 1, size, i, compar);
     }
+
+    heapify(base, nelem - 1, size, 0, compar);
     
-    for (i = nelem; i > 1; i--)
+
+    for (i = nelem - 1; i > 0; i--)
     {
-        swap(base + size, base + i * size, size);
-        heapify(base, i - 1, size, 1, compar);
+        swap(base, base + i * size, size);
+        heapify(base, i - 1, size, 0, compar);
     }
 }
 
-void heapify(char *base, size_t nelem, size_t size, size_t parent, int (*compar)(const void *, const void *))
+void heapify(void *base, size_t nelem, size_t size, size_t parent, int (*compar)(const void *, const void *))
 {
     size_t child;
-    while ((child = 2 * parent) <= nelem)
+    while ((child = 2 * parent + 1) <= nelem)
     {
         if (child + 1 <= nelem && compar(base + child * size, base + (child + 1) * size) < 0)
         {
-            child = child + 1;
+            child = 2 * parent + 2;
         }
+        
         if (compar(base + parent * size, base + child * size) < 0)
         {
             swap(base + parent * size, base + child * size, size);
